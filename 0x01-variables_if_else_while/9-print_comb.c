b28ef63878d9690d9c7fbb92e3872b5918eb5797
Ogotilillian <ogotilillian3@gmail.com>
2023-07-15 13:10:43 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,19 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
-{	
-int n;
-
-for (n = 48; n <= 59; n++)
 {
-	putchar(n);
-	if (n == 57)
-	{
-	continue;
-	}
-	putchar(',');
-	putchar(' ');
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n == 57)
+{
+continue;
 }
-	putchar('\n');
-	return (0);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
