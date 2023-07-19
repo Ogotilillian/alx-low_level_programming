6648a520aad143fe303eb3bc2bc728b3f72b78f3
Ogotilillian <ogotilillian3@gmail.com>
2023-07-19 11:23:17 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,32 +12,26 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
+			_putchar('0');
 			for (b = 1; b <= n; b++)
 			{
+				_putchar(',');
+				_putchar(' ');
 				c = a * b;
-				if (c < 10)
+				if (c < 99)
+					_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
+				if (c >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(c + '0');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10)) % 10 + '0');
 				}
-				else if (c >= 10 && c < 100)
+				else if (c <= 99 && c >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(c / 10 + '0');
-					_putchar(c % 10 + '0');
+					_putchar((c / 10) + '0');
 				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(c / 100 + '0');
-					_putchar((c / 10) % 10 + '0');
-					_putchar(c % 10 + '0');
-				}
+				_putchar((c % 10) + '0');
 			}
 			_putchar('\n');
 		}
