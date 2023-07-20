31dd5392a4fb1ff14bc271ae0ec19f7f16990623
Ogotilillian <ogotilillian3@gmail.com>
2023-07-20 12:03:07 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
 	/**
 	*while (i < 10)
@@ -18,6 +18,6 @@ int main(void)
 	*	putchar(i);
 	*}
 	*/
-	printf("Infinite loop avoided! \\o/\n");	
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
