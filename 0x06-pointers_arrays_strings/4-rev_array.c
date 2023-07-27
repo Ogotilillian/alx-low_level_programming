cae4ec53dfecc80e1951e4435f5f7644e60b7b43
Ogotilillian <ogotilillian3@gmail.com>
2023-07-27 11:19:31 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,8 +4,13 @@
   * reverse_array - function that reverses the content of an array of integers.
   * @a: pointer to array
   * @n: numberof elements of array
-  * Return: void
+  * This function takes an array of integers and its size as input
+  * and reverses the order of its elements in-place.
   *
+  * Example:
+  * int arr[] = {1, 2, 3, 4, 5};
+  * reverse_array(arr, 5);
+  * // The array arr will now be: {5, 4, 3, 2, 1}
   */
 
 void reverse_array(int *a, int n)
