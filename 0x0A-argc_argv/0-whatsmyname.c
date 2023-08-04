387540349985d762db122c32971ffcc2dd1cbf19
Ogotilillian <ogotilillian3@gmail.com>
2023-08-04 13:48:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: stands for argument count
+ * @argv: stands for argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
