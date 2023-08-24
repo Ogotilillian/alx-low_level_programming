ab010e306c0d4666421bab7fc14a30f6a3d47006
Ogotilillian <ogotilillian3@gmail.com>
2023-08-24 12:36:14 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-		while (h)
+		while (h != NULL)
 		{
 			if (h->str == NULL)
 				printf("[0] (nil)\n");
 			else
 				printf("[%u} %s\n", h->len, h->str);
-			i++;
 			h = h->next;
+			i++;
 		}
 	return (i);
 }
